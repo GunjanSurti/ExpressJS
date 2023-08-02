ee0d27df0ae8a7de132de1380475aa4986146efc
GunjanSurti <gunjansurti142@gmail.com>
2023-08-02 18:02:47 +0530

    Routes

---
+++ routers/index.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+
+router.get("/", (req, res) => {
+    res.render(
+      "index",
+      /*passing obj*/ {
+        title: "render server",
+      }
+    );
+    // no extention
+    //it takes path to the "view" file without extension
+    // it starts looking for file in given folder in our case it is 'views'
+    router.get("/about", (req, res) => {
+        // res.send("<h1>Hello from Express</h1> ")
+        //   res.sendFile(path.resolve(__dirname) + "/views/about.html");
+        res.render("about", {
+          title: "Server About",
+        });
+      });
+  });
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const mainRouters = require("./routers/index");
 
 // this will set key:value in app container
 // set("key","value")
@@ -11,32 +12,33 @@ app.set("view engine", "ejs");
 // static middleware
 app.use(express.static("public")); // public has static files
 
+app.use(mainRouters );
 // PORT=5000 node server.js or server only, this will run on port 5000
 app.listen(PORT, () => {
   console.log(`Callback, Listening on port ${PORT}`);
 });
-// app.get("/aouut",()=>{}) to go to about page
-app.get("/", (req, res) => {
-  // res.send("<h1>Hello from Express</h1> ")
-  //   res.sendFile(path.resolve(__dirname + "/views/index.html"));
-  res.render(
-    "index",
-    /*passing obj*/ {
-      title: "render server",
-    }
-  );
-  // no extention
-  //it takes path to the "view" file without extension
-  // it starts looking for file in given folder in our case it is 'views'
-});
+// app.get("/about",()=>{}) to go to about page
+// app.get("/", (req, res) => {
+//   // res.send("<h1>Hello from Express</h1> ")
+//   //   res.sendFile(path.resolve(__dirname + "/views/index.html"));
+//   res.render(
+//     "index",
+//     /*passing obj*/ {
+//       title: "render server",
+//     }
+//   );
+//   // no extention
+//   //it takes path to the "view" file without extension
+//   // it starts looking for file in given folder in our case it is 'views'
+// });
 
-app.get("/about", (req, res) => {
-  // res.send("<h1>Hello from Express</h1> ")
-  //   res.sendFile(path.resolve(__dirname) + "/views/about.html");
-  res.render("about", {
-    title: "Server About",
-  });
-});
+// app.get("/about", (req, res) => {
+//   // res.send("<h1>Hello from Express</h1> ")
+//   //   res.sendFile(path.resolve(__dirname) + "/views/about.html");
+//   res.render("about", {
+//     title: "Server About",
+//   });
+// });
 
 // to download file
 app.get("/download", (req, res) => {
